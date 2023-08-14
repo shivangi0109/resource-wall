32b37dcaf98c4b3d19b743aff2dfbdb67bfcddb4
Devvaru <syeraru@gmail.com>
2023-08-14 15:27:45 -0600

    Create table files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,14 @@
 -- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+
+CREATE TABLE users
+(
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  -- password VARCHAR(255) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  profile_image_url VARCHAR(255),
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  bio VARCHAR(255)
 );
