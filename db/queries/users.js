9681c4d4869b046f3e359611a223bf9392f309ef
Shivangi Srivastava <shi06dec@gmail.com>
2023-08-18 17:56:29 -0600

    Implemented my resources

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,18 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+const getUserById = (id) => {
+  return db.query('SELECT * FROM users WHERE id = $1;', [id])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+const getUsersResources = (id) => {
+  return db.query('SELECT users.*, resources.* FROM users JOIN resources ON users.id = resources.user_id WHERE users.id = $1;', [id])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getUsers, getUserById, getUsersResources };
