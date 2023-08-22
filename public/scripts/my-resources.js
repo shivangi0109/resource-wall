7f7ab3f002a5806bc60c3941778a20e2015b14a6
Shivangi Srivastava <shi06dec@gmail.com>
2023-08-22 15:27:34 -0600

    Fix bug for my-resources scripts

---
+++ public/scripts/my-resources.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // toggles my resources
+ $('.button-left').on('click', function() {
+   $('#my-resources').slideDown();
+   $('#my-favourites').slideUp();
+   $('.button-left').addClass("button-active");
+   $('.button-right').removeClass("button-active");
+ });
+
+ // toggles my favourites
+ $('.button-right').on('click', function() {
+   $('#my-favourites').slideDown({
+     start: function () {
+       $(this).removeClass("hidden");
+       $(this).css({
+         display: "flex"
+       });
+     }
+   });
+   $('#my-resources').slideUp();
+   $('.button-right').addClass("button-active");
+   $('.button-left').removeClass("button-active");
+ });
+});
