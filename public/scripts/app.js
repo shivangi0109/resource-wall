716eecaa0db5328894100bb19ffd169c91903c9a
Shivangi Srivastava <shi06dec@gmail.com>
2023-08-21 20:53:03 -0600

    Added moment library to display date and time format in resource show page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,14 @@
 // Client facing scripts here
 
 $(document).ready(function() {
+  var createdOn = $("#formattedDate").text(); // Get the timestamp from the paragraph
+
+  // Use Moment.js to format the timestamp
+  var formattedDate = moment(createdOn).startOf('hour').fromNow();
+
+  // Update the content of the paragraph with the formatted date
+  $("#formattedDate").text("Created on: " + formattedDate);
+
   // Add click event to the heart button
   $("#heartButton").on('click', function(event) {
     event.preventDefault();
