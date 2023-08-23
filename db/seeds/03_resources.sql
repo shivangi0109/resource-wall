5ecdd8a89c5e003041b09cd5cdd3ae75293fe9a3
karvok <kayvee123@gmail.com>
2023-08-23 10:31:04 -0600

    Add additional resources seeds

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,46 +1,34 @@
 INSERT INTO resources (user_id, category_id, title, description, resource_url, thumbnail_url)
 VALUES
-
 (1, 10, 'WordPress', 'Building a website is a journey, and you''ll continue to tweak it along the way. To help you achieve your goals, there are several approaches you can take. The intent of this site is to make access to resources as convenient as possible to align with your preferred method of learning and exploring.', 'https://wordpress.com/', 'https://images.pexels.com/photos/4005569/pexels-photo-4005569.jpeg'),
-
 (1, 9, 'Reverb', 'The Guitar Store', 'https://reverb.com/au/', 'https://images.pexels.com/photos/1407322/pexels-photo-1407322.jpeg'),
-
 (1, 9, 'Ableton', 'Inspire young people to learn music through the excitement of making their own. Get instruments, support and ready-to-use resources for your education organization, and put the thrill of making music at the center of your class.', 'https://www.ableton.com/en/', 'https://images.pexels.com/photos/15396453/pexels-photo-15396453.jpeg'),
-
 (1, 2, 'Yummly', 'Yummly''s proprietary Food Genome and patent-pending Food Intelligence technology allow us to understand recipes at a deeper level, and recommend recipes to our users based on their diets, allergies, tastes, and more.', 'https://www.yummly.com/', 'https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg'),
-
 (1, 3, 'Figma', 'Figma is an online design tool for riffing on, sharing, and creating the best work, together.', 'https://www.figma.com/', 'https://images.pexels.com/photos/196646/pexels-photo-196646.jpeg'),
-
 (1, 2, 'All Recipes', 'Most importantly, Allrecipes connects home cooks with their greatest sources of inspiration — other home cooks. We''re the world''s leading digital food brand, and that inspires us to do everything possible to keep our community connected. Sixty-million home cooks deserve no less.', 'https://www.allrecipes.com/', 'https://images.pexels.com/photos/2267868/pexels-photo-2267868.jpeg'),
-
 (1, 5, 'Plant with Purpose', 'At Plant With Purpose, we restore hope by reversing two of the world''s greatest challenges: global poverty and environmental damage. These problems are interconnected. Our goal is to see creation''s life-giving regenerative cycles restored and people renewed through their relationship to God, living in healthy sufficiency and empowered to dream.', 'https://plantwithpurpose.org/', 'https://images.pexels.com/photos/797797/pexels-photo-797797.jpeg'),
-
 (1, 7, 'Duolingo', 'Learning with Duolingo is fun, and research shows that it works! With quick, bite-sized lessons, you''ll earn points and unlock new levels while gaining real-world communication skills.', 'https://www.duolingo.com/', 'https://images.pexels.com/photos/267669/pexels-photo-267669.jpeg'),
-
 (1, 6, 'Planet Fitness', 'Looking for a little extra guidance or inspiration to help you meet your fitness goals? As a PF member, you can take advantage of our free fitness training or find your motivation by reading real member experiences on Planet of Triumphs. It''s our goal to provide a clean, safe, welcoming environment for anyone who walks through our door, and all the equipment, amenities and support you need once you''re here.', 'https://www.planetfitness.ca/', 'https://images.pexels.com/photos/221247/pexels-photo-221247.jpeg'),
-
 (1, 3, 'Interior Design Institute', 'At the Interior Design Institute, we don''t just teach interior design. We encourage every student to access and nurture their own natural sense of flair and creativity. We also teach the technical and practical skills necessary to translate those design ideas into reality.', 'https://www.theinteriordesigninstitute.ca/', 'https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg'),
-
 (1, 1, 'Art Gallery of Alberta', 'Collecting is central to the Art Gallery of Alberta''s role as an active resource and research facility for the Alberta community and broader public. Since its founding in 1924, the AGA has acquired over 6,000 works of art, the vast majority of which were produced after 1950 and added to the Collection since 1970. The strength of the gallery''s contemporary collection lies primarily in the area of Canadian abstract painting and sculpture, with a strong emphasis on the work of artists from this region in the context of parallel national and international movements.', 'https://www.youraga.ca/', 'https://images.pexels.com/photos/2086361/pexels-photo-2086361.jpeg'),
-
-(1, 1, 'MoMA', 'Museum of Modern Art', 'https://www.moma.org/', 'https://images.pexels.com/photos/2807495/pexels-photo-2807495.jpeg'),
-
+(2, 1, 'MoMA', 'Museum of Modern Art', 'https://www.moma.org/', 'https://images.pexels.com/photos/2807495/pexels-photo-2807495.jpeg'),
 (1, 2, 'Jun''s Kitchen', 'A relaxing cooking channel sharing popular Japanese recipes', 'https://www.youtube.com/channel/UCRxAgfYexGLlu1WHGIMUDqw', 'https://images.pexels.com/photos/6896514/pexels-photo-6896514.jpeg'),
-
 (1, 3, 'Dribbble', 'Millions of designers and agencies around the world showcase their portfolio work on Dribbble - the home to the world''s best design and creative professionals.', 'https://dribbble.com/', 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg'),
-
-(1, 4, 'Financial Education Resources', 'An annotated list of Canadian and international websites that provide financial information, on topics such as inflation, banking, personal finances, investing and consumer protection.', 'https://www.bankofcanada.ca/about/educational-resources/financial-education-resources/', 'https://images.pexels.com/photos/4386158/pexels-photo-4386158.jpeg'),
-
-(2, 5, 'Garden Design', 'Learn How to Garden', 'https://www.gardendesign.com/how-to/', 'https://images.pexels.com/photos/16254627/pexels-photo-16254627.jpeg'),
-
-(2, 6, 'Be Healthy', 'Take a Spa Retreat!', 'https://www.sparklinghill.com/', 'https://images.pexels.com/photos/4916534/pexels-photo-4916534.jpeg'),
-
-(2, 7, 'Language', 'Parlez-vous français?', 'https://www.rosettastone.com', 'https://images.pexels.com/photos/7450476/pexels-photo-7450476.jpeg'),
-
+(1, 4, 'Financial Education', 'An annotated list of Canadian and international websites that provide financial information, on topics such as inflation, banking, personal finances, investing and consumer protection.', 'https://www.bankofcanada.ca/about/educational-resources/financial-education-resources/', 'https://images.pexels.com/photos/4386158/pexels-photo-4386158.jpeg'),
+(2, 5, 'Garden Design', 'Learn how to plant and care for vegetables, flowers, and more', 'https://www.gardendesign.com/how-to/', 'https://images.pexels.com/photos/16254627/pexels-photo-16254627.jpeg'),
+(2, 6, 'Sparkling Hill Health & Wellness Resort', 'Disconnect from the world and reconnect with yourself', 'https://www.sparklinghill.com/', 'https://images.pexels.com/photos/4916534/pexels-photo-4916534.jpeg'),
+(2, 7, 'Rosetta Stone', 'Get conversation-ready in 25 languages', 'https://www.rosettastone.com', 'https://images.pexels.com/photos/7450476/pexels-photo-7450476.jpeg'),
 (3, 8, 'Marketing Is Everything', 'The 1990s will belong to the customer. And that is great news for the marketer. Technology is transforming choice, and choice is transforming the marketplace. As a result, we are witnessing the emergence of a new marketing paradigm—not a “do more” marketing that simply turns up the volume on the sales spiels of the past but a knowledge- and experience-based marketing that represents the once-and-for-all death of the salesman.', 'https://hbr.org/1991/01/marketing-is-everything', 'https://images.pexels.com/photos/6476587/pexels-photo-6476587.jpeg'),
-
-(3, 9, 'The power of music: how it can benefit health', 'I think music in itself is healing, American musician Billy Joel once said. Its an explosive expression of humanity. Its something we are all touched by. No matter what culture we are from, everyone loves music.', 'https://www.medicalnewstoday.com/articles/302903', 'https://images.pexels.com/photos/13244401/pexels-photo-13244401.jpeg'),
-
+(3, 9, 'Power of Music: it can benefit health', 'I think music in itself is healing, American musician Billy Joel once said. Its an explosive expression of humanity. Its something we are all touched by. No matter what culture we are from, everyone loves music.', 'https://www.medicalnewstoday.com/articles/302903', 'https://images.pexels.com/photos/13244401/pexels-photo-13244401.jpeg'),
 (3, 10, 'NewScientist Technology', 'From the earliest stone tools to the latest advances in artificial intelligence, robotics, blockchains and virtual reality, technology has continuously transformed our lives. Stay informed about the latest technological breakthroughs and the ethical and societal norms they challenge.', 'https://www.newscientist.com/subject/technology/', 'https://images.pexels.com/photos/10556474/pexels-photo-10556474.jpeg'),
-
-(3, 11, 'Search Engine', 'Google all the things!', 'https://www.google.com/', 'https://images.pexels.com/photos/6446709/pexels-photo-6446709.jpeg');
+(3, 11, 'IKEA Hacks', 'Explore these fantastic new DIY projects for the whole family!', 'https://ikeahackers.net/', 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg'),
+(2, 2, 'Manjula''s Kitchen', 'Indian Vegetarian Recipes and delicious Cooking Videos', 'https://www.manjulaskitchen.com/', 'https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg'),
+(2, 6, 'What is Pilates?', 'A complete guide for beginners', 'https://www.womenshealthmag.com/uk/fitness/workouts/a707014/best-pilates-youtube-workouts/', 'https://images.pexels.com/photos/4498609/pexels-photo-4498609.jpeg'),
+(2, 7, 'Babbel', 'You''re just a click away from learning a new language', 'https://www.babbel.com/', 'https://images.pexels.com/photos/21627/pexels-photo.jpg'),
+(2, 2, 'Food Wishes', 'Video Recipes with Chef John', 'https://foodwishes.blogspot.com/', 'https://images.pexels.com/photos/8018079/pexels-photo-8018079.jpeg'),
+(3, 10, 'From Nand to Tetris', 'Lectures, project materials, and tools for building a general-purpose computer system', 'https://www.nand2tetris.org/', 'https://images.pexels.com/photos/6636497/pexels-photo-6636497.jpeg'),
+(3, 3, 'Brutalism is Back', 'Why Brutalism Is Making an Interior-Design Comeback', 'https://www.vogue.com/article/why-brutalism-is-making-a-comeback-in-2023', 'https://images.pexels.com/photos/17550471/pexels-photo-17550471/free-photo-of-stairs-and-brutalist-architecture.jpeg'),
+(3, 5, 'Houseplants', 'Add beautiful foliage and flowers to just about any room in your home', 'https://www.bhg.com/gardening/houseplants/', 'https://images.pexels.com/photos/1974508/pexels-photo-1974508.jpeg'),
+(2, 3, 'What is Color Theory?', 'Learn to apply color theory to optimize a positive psychological impact on users', 'https://www.interaction-design.org/literature/topics/color-theory', 'https://images.pexels.com/photos/1146851/pexels-photo-1146851.jpeg'),
+(2, 3, 'Maximalism - Because more is more', 'Bold use of color, pattern, and layering to create a space that honors and celebrates excess', 'https://www.architecturaldigest.com/story/maximalist-interior-design-101', 'https://images.pexels.com/photos/6434613/pexels-photo-6434613.jpeg'),
+(1, 11, 'Frog Forum', 'Great advice, friendly people, and lots of frogs!', 'https://www.frogforum.net/', 'https://images.pexels.com/photos/57727/tree-frog-anuran-frog-amphibians-57727.jpeg');
