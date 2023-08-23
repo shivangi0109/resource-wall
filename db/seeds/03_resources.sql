5aefed1c49f1affccc92caa7cd414a9ff43d535d
Devvaru <syeraru@gmail.com>
2023-08-22 18:45:20 -0600

    Add 10 more resources

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,5 +1,26 @@
 INSERT INTO resources (user_id, category_id, title, description, resource_url, thumbnail_url)
 VALUES
+
+(1, 10, 'WordPress', 'Building a website is a journey, and you''ll continue to tweak it along the way. To help you achieve your goals, there are several approaches you can take. The intent of this site is to make access to resources as convenient as possible to align with your preferred method of learning and exploring.', 'https://wordpress.com/', 'https://images.pexels.com/photos/4005569/pexels-photo-4005569.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 9, 'Reverb', 'The Guitar Store', 'https://reverb.com/au/', 'https://images.pexels.com/photos/1407322/pexels-photo-1407322.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 9, 'Ableton', 'Inspire young people to learn music through the excitement of making their own. Get instruments, support and ready-to-use resources for your education organization, and put the thrill of making music at the center of your class.', 'https://www.ableton.com/en/', 'https://images.pexels.com/photos/15396453/pexels-photo-15396453.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 2, 'Yummly', 'Yummly''s proprietary Food Genome and patent-pending Food Intelligence technology allow us to understand recipes at a deeper level, and recommend recipes to our users based on their diets, allergies, tastes, and more.', 'https://www.yummly.com/', 'https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 3, 'Figma', 'Figma is an online design tool for riffing on, sharing, and creating the best work, together.', 'https://www.figma.com/', 'https://images.pexels.com/photos/196646/pexels-photo-196646.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 2, 'All Recipes', 'Most importantly, Allrecipes connects home cooks with their greatest sources of inspiration — other home cooks. We''re the world''s leading digital food brand, and that inspires us to do everything possible to keep our community connected. Sixty-million home cooks deserve no less.', 'https://www.allrecipes.com/', 'https://images.pexels.com/photos/2267868/pexels-photo-2267868.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 5, 'Plant with Purpose', 'At Plant With Purpose, we restore hope by reversing two of the world''s greatest challenges: global poverty and environmental damage. These problems are interconnected. Our goal is to see creation''s life-giving regenerative cycles restored and people renewed through their relationship to God, living in healthy sufficiency and empowered to dream.', 'https://plantwithpurpose.org/', 'https://images.pexels.com/photos/797797/pexels-photo-797797.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 7, 'Duolingo', 'Learning with Duolingo is fun, and research shows that it works! With quick, bite-sized lessons, you''ll earn points and unlock new levels while gaining real-world communication skills.', 'https://www.duolingo.com/', 'https://images.pexels.com/photos/267669/pexels-photo-267669.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 6, 'Planet Fitness', 'Looking for a little extra guidance or inspiration to help you meet your fitness goals? As a PF member, you can take advantage of our free fitness training or find your motivation by reading real member experiences on Planet of Triumphs. It''s our goal to provide a clean, safe, welcoming environment for anyone who walks through our door, and all the equipment, amenities and support you need once you''re here.', 'https://www.planetfitness.ca/', 'https://images.pexels.com/photos/221247/pexels-photo-221247.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
+(1, 3, 'Interior Design Institute', 'At the Interior Design Institute, we don''t just teach interior design. We encourage every student to access and nurture their own natural sense of flair and creativity. We also teach the technical and practical skills necessary to translate those design ideas into reality.', 'https://www.theinteriordesigninstitute.ca/', 'https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&h=300'),
+
 (1, 1, 'Art Gallery of Alberta', 'Collecting is central to the Art Gallery of Alberta''s role as an active resource and research facility for the Alberta community and broader public. Since its founding in 1924, the AGA has acquired over 6,000 works of art, the vast majority of which were produced after 1950 and added to the Collection since 1970. The strength of the gallery''s contemporary collection lies primarily in the area of Canadian abstract painting and sculpture, with a strong emphasis on the work of artists from this region in the context of parallel national and international movements.', 'https://www.youraga.ca/', 'https://images.pexels.com/photos/2086361/pexels-photo-2086361.jpeg?auto=compress&cs=tinysrgb&h=300'),
 (1, 1, 'MoMA', 'Museum of Modern Art', 'https://www.moma.org/', 'https://images.pexels.com/photos/2807495/pexels-photo-2807495.jpeg?auto=compress&cs=tinysrgb&h=300'),
 (1, 2, 'Jun''s Kitchen', 'A relaxing cooking channel sharing popular Japanese recipes', 'https://www.youtube.com/channel/UCRxAgfYexGLlu1WHGIMUDqw', 'https://images.pexels.com/photos/6896514/pexels-photo-6896514.jpeg?auto=compress&cs=tinysrgb&h=300'),
